a67d04855c55f497645baf59d15a31413b0f64e7
Brap098 <Brapagon.Work@gmail.com>
2022-05-03 20:53:34 -0400

    B change

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(assets,left, bottom){
+    let pic = document.createElement('img')
+pic.src = assets
+pic.style.position = 'fixed'
+pic.style.left = left + 'px'
+pic.style.bottom = bottom + 'px'
+document.body.append(pic)
+return pic
+}
+function newItem(assets, left, bottom ){
+    let item = newImage(assets,left, bottom)
+ 
+    document.body.append(item)
+    
+    item.addEventListener('dblclick', function(){
+        item.remove()
+    })
+       
+}
+
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/tree.png', 175, 200)
+newImage('assets/pillar.png', 300, 50)
+newImage('assets/rock.png', 50, 150)
+newImage('assets/well.png', 150, 120)
+
+
+newItem('assets/sword.png', 480, 200)
+newItem('assets/staff.png',175,250 )
+newItem('assets/sheild.png', 345, 290)
